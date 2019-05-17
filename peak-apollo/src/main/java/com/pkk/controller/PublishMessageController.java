85b7523101cc3cad56e1b30bd27e303f8254e1f9
裴昆昆 <656678879@qq.com>
2019-05-17 18:36:31 +0800

    修改配置

--- peak-apollo/src/main/java/com/pkk/controller/PublishMessageController.java
+++ peak-apollo/src/main/java/com/pkk/controller/PublishMessageController.java
@@ -1,7 +1,12 @@
 package com.pkk.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
 import com.pkk.message.LoggerMessage;
 import com.pkk.queue.LoggerQueue;
+import java.util.HashMap;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +20,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("publish")
 public class PublishMessageController {
+
+  @ApolloConfig
+  private Config config;
+
+
+  /**
+   * @Description: 获取配置信息
+   * @Param: []
+   * @return: java.lang.Object
+   * @Author: peikunkun
+   * @Date: 2019/5/17 0017 上午 9:31
+   */
+  @RequestMapping("getConfig")
+  public Object getConfig() {
+    Set<String> propertyNames = config.getPropertyNames();
+    HashMap<Object, Object> configMap = new HashMap<>();
+    propertyNames.forEach(key -> {
+      configMap.put(key, config.getProperty(key, "无任何配置"));
+      System.err.println(key + "=" + config.getIntProperty(key, 0));
+    });
+    return JSONObject.toJSONString(configMap);
+  }
+
 
   /**
    * @Description: 发送消息
