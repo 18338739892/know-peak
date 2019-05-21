f3e77dc75b797080bd1fbabf4c1e44153c2b1766
裴昆昆 <656678879@qq.com>
2019-05-21 10:43:29 +0800

    动态修改配置的修改

--- peak-apollo/src/main/java/com/pkk/PeakApolloApplication.java
+++ peak-apollo/src/main/java/com/pkk/PeakApolloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 /**
- * @description: 启动类
+ * @description: 启动类【动态修改配置可依据:https://blog.csdn.net/qq_39470733/article/details/88632566】
  * @author: peikunkun
  * @create: 2019-05-14 16:19
  **/
