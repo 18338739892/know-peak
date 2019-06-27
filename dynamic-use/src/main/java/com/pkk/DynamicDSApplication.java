f2cb0d635b266b112deba48acfcf190600dee73b
裴昆昆 <656678879@qq.com>
2019-06-27 18:40:03 +0800

    迁移数据源切换到support

--- dynamic-use/src/main/java/com/pkk/DynamicDSApplication.java
+++ dynamic-use/src/main/java/com/pkk/DynamicDSApplication.java
@@ -1,10 +1,12 @@
 package com.pkk;
 
 import com.pkk.penguin.support.dynamic.datasource.EnableDynamicDB;
+import com.pkk.penguin.support.dynamic.datasource.properties.DynamicSSHProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
--- dynamic-use/src/main/java/com/pkk/dynamic/use/config/GetDataSource.java
+++ dynamic-use/src/main/java/com/pkk/dynamic/use/config/GetDataSource.java
@@ -1,9 +1,15 @@
 package com.pkk.dynamic.use.config;
 
+import com.jcraft.jsch.JSchException;
 import com.pkk.dynamic.use.service.DataSourceService;
+import com.pkk.penguin.support.dynamic.datasource.common.InitSshContent;
+import com.pkk.penguin.support.dynamic.datasource.common.SSHConnection;
 import com.pkk.penguin.support.dynamic.datasource.exception.DynamicDataSourceException;
 import com.pkk.penguin.support.dynamic.datasource.exception.code.DyDataSourceCode;
+import com.pkk.penguin.support.dynamic.datasource.properties.DynamicSSHConnectionProperties;
+import com.pkk.penguin.support.dynamic.datasource.properties.DynamicSSHProperties;
 import com.pkk.penguin.support.dynamic.datasource.strategy.DataSourceGetStrategy;
+import com.pkk.penguin.support.dynamic.datasource.support.SSHConnectionSupport;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.commons.lang3.StringUtils;
@@ -11,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
+
 
 /**
  * 获取数据源
@@ -19,54 +25,71 @@ import java.sql.SQLException;
 @Configuration
 public class GetDataSource extends DataSourceGetStrategy {
 
-    @Autowired
-    private DataSourceService dataSourceService;
+  @Autowired
+  private DataSourceService dataSourceService;
+  @Autowired
+  private DynamicSSHProperties dynamicSSHProperties;
+  @Autowired
+  private SSHConnectionSupport sshConnectionSupport;
+  @Autowired
+  private SSHConnection sshConnection;
 
-    /**
-     * <p>Title: getDataSource<／p>
-     * <p>Description: 获取数据源的策略<／p>
-     * <p>Copyright: Copyright (c) 2018<／p>
-     *
-     * @author pkk
-     * @date 2019/3/19 0019
-     * @version 1.0
-     */
-    @Override
-    public DataSource getDataSource(String dataSourceKey) {
-
-        com.pkk.dynamic.use.entity.DataSource entity = dataSourceService.selectDbInfoByDbName(dataSourceKey);
-        if (null == entity) {
-            throw new DynamicDataSourceException(DyDataSourceCode.DATABASEERROR.getCode(),
-                    "不存在此:[" + dataSourceKey + "],数据源");
-        }
-
-        //连接操作
-        String driverClassName = "com.mysql.cj.jdbc.Driver";
-        String validationQuery = "select 1";
-        String url = "jdbc:mysql://%s:%s/%s";
-        url = String.format(url, entity.getDbIp(), entity.getDbPort(), entity.getDbName());
-        if (StringUtils.isNotBlank(entity.getParameters())) {
-            url += "?" + entity.getParameters();
-        }
-
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(url); //数据源
-        config.setUsername(entity.getUsername()); //用户名
-        config.setPassword(entity.getPassword()); //密码
-        config.setDriverClassName(driverClassName);
-        config.setConnectionTimeout(1000 * 60);
-        config.setConnectionTestQuery(validationQuery);
-        config.addDataSourceProperty("cachePrepStmts", "true"); //是否自定义配置，为true时下面两个参数才生效
-        config.addDataSourceProperty("prepStmtCacheSize", "250"); //连接池大小默认25，官方推荐250-500
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048"); //单条语句最大长度默认256，官方推荐2048
-        config.addDataSourceProperty("useServerPrepStmts", "true"); //新版本MySQL支持服务器端准备，开启能够得到显著性能提升
-        config.addDataSourceProperty("useLocalSessionState", "true");
-        config.addDataSourceProperty("useLocalTransactionState", "true");
-        config.addDataSourceProperty("rewriteBatchedStatements", "true");
-        config.addDataSourceProperty("cacheResultSetMetadata", "true");
-        config.addDataSourceProperty("cacheServerConfiguration", "true");
-        config.addDataSourceProperty("elideSetAutoCommits", "true");
-        config.addDataSourceProperty("maintainTimeStats", "false");
-        return new HikariDataSource(config);
+  /**
+   * <p>Title: getDataSource<／p>
+   * <p>Description: 获取数据源的策略<／p>
+   * <p>Copyright: Copyright (c) 2018<／p>
+   *
+   * @author pkk
+   * @date 2019/3/19 0019
+   * @version 1.0
+   */
+  @Override
+  public DataSource getDataSource(String dataSourceKey) {
+    com.pkk.dynamic.use.entity.DataSource entity = dataSourceService.selectDbInfoByDbName(dataSourceKey);
+    if (null == entity) {
+      throw new DynamicDataSourceException(DyDataSourceCode.DATABASEERROR.getCode(),
+          "不存在此:[" + dataSourceKey + "],数据源");
     }
+
+    //更换sship的操作
+    if (dynamicSSHProperties.isEnable()) {
+      final DynamicSSHConnectionProperties dynamicSSHConnectionProperties = InitSshContent.USE_MYSQL_INFO.get();
+      try {
+        sshConnectionSupport
+            .resetPortForwarding(sshConnection, dynamicSSHConnectionProperties.getLocalPort(), "127.0.0.1", 3306);
+      } catch (JSchException e) {
+        e.printStackTrace();
+        throw new RuntimeException("切换ssh失败!");
+      }
+    }
+
+    //连接操作
+    String driverClassName = "com.mysql.cj.jdbc.Driver";
+    String validationQuery = "select 1";
+    String url = "jdbc:mysql://%s:%s/%s";
+    url = String.format(url, entity.getDbIp(), entity.getDbPort(), entity.getDbName());
+    if (StringUtils.isNotBlank(entity.getParameters())) {
+      url += "?" + entity.getParameters();
+    }
+
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(url); //数据源
+    config.setUsername(entity.getUsername()); //用户名
+    config.setPassword(entity.getPassword()); //密码
+    config.setDriverClassName(driverClassName);
+    config.setConnectionTimeout(1000 * 60);
+    config.setConnectionTestQuery(validationQuery);
+    config.addDataSourceProperty("cachePrepStmts", "true"); //是否自定义配置，为true时下面两个参数才生效
+    config.addDataSourceProperty("prepStmtCacheSize", "250"); //连接池大小默认25，官方推荐250-500
+    config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048"); //单条语句最大长度默认256，官方推荐2048
+    config.addDataSourceProperty("useServerPrepStmts", "true"); //新版本MySQL支持服务器端准备，开启能够得到显著性能提升
+    config.addDataSourceProperty("useLocalSessionState", "true");
+    config.addDataSourceProperty("useLocalTransactionState", "true");
+    config.addDataSourceProperty("rewriteBatchedStatements", "true");
+    config.addDataSourceProperty("cacheResultSetMetadata", "true");
+    config.addDataSourceProperty("cacheServerConfiguration", "true");
+    config.addDataSourceProperty("elideSetAutoCommits", "true");
+    config.addDataSourceProperty("maintainTimeStats", "false");
+    return new HikariDataSource(config);
+  }
 }
